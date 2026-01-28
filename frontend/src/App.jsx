e7b14761f6e0096166a8db838083c1cb466663ef
pankaj rathod <pankajrathod272004@gmail.com>
2026-01-28 14:54:05 +0530

    setup react and created basic pages and install tailwind css

---
+++ frontend/src/App.jsx
@@ -0,0 +1,21 @@
+import { Routes, Route } from 'react-router';
+import HomePage from './pages/HomePage';
+import CreatePage from './pages/CreatePage';
+import NoteDetailPage from './pages/NoteDetailPage';
+import toast, { Toaster } from 'react-hot-toast';
+
+const App = () => {
+  return (
+    <div>
+      <Toaster />
+      <button onClick={() => toast.success("congrats")}> click</button>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/create" element={<CreatePage />} />
+        <Route path="/note/:id" element={<NoteDetailPage />} />
+      </Routes>
+    </div>
+  );
+};
+
+export default App
